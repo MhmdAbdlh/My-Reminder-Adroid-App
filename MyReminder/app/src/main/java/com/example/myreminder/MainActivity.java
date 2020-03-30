01d14d695d98f3b69acb9ad380e81e335da75daf
Mouhammdkhier <eng.mhd.sh@gmail.com>
2020-03-31 01:48:14 +0300

    dialog added

--- MyReminder/app/src/main/java/com/example/myreminder/MainActivity.java
+++ MyReminder/app/src/main/java/com/example/myreminder/MainActivity.java
@@ -36,12 +36,18 @@ public class MainActivity extends AppCompatActivity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                PopupMenu popup = new PopupMenu(MainActivity.this, view);
-                popup.getMenuInflater().inflate(R.menu.menu,popup.getMenu());
-                popup.show();
+//                PopupMenu popup = new PopupMenu(MainActivity.this, view);
+//                popup.getMenuInflater().inflate(R.menu.menu,popup.getMenu());
+//                popup.show();
+                openDialog();
             }
         });
 
 
+    }
+
+    public void openDialog() {
+        Edit_dialog dialog = new Edit_dialog();
+        dialog.show(getSupportFragmentManager(), "edit Reminder");
     }
 }
