d96e7bf790352705b0fab1ac4ebff9a4208a6fe4
MhmdAbdlh <47702978+MhmdAbdlh@users.noreply.github.com>
2020-03-31 00:49:25 +0200

    Drop-down menu

--- MyReminder/app/src/main/java/com/example/myreminder/MainActivity.java
+++ MyReminder/app/src/main/java/com/example/myreminder/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myreminder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,15 +26,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ListView list = findViewById(R.id.ReminderList);
         ArrayList <String> myList = new ArrayList <>();
-        myList.add("shj");
-        myList.add("shj");
-        myList.add("shj");
-        myList.add("shj");
-        myList.add("shj");
-        myList.add("shj");
-        myList.add("shj");
-        myList.add("shj");
-        myList.add("shj");
+        myList.add("reminder1");
+        myList.add("reminder1");
+        myList.add("reminder1");
+        myList.add("reminder1");
+        myList.add("reminder1");
+        myList.add("reminder1");
+        myList.add("reminder1");
+        myList.add("reminder1");
+        myList.add("reminder1");
         ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,myList);
         list.setAdapter(arrayAdapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -42,6 +46,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.drop_down_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.exit:
+                onDestroy();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
